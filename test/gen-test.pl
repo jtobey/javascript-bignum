2cf60f43628cdb23eb22a6ddc4b2496594c5f6e0
John Tobey <John.Tobey@gmail.com>
2012-03-16 02:02:43 -0400

    Made test suite run under Node.js, 95% speedup versus Rhino.

--- test/gen-test.pl
+++ test/gen-test.pl
@@ -195,8 +195,19 @@ sub gen_scheme {
 
 sub prolog_js {
     print(<<'END');
-load("../biginteger.js");
-load("../schemeNumber.js");
+var BigInteger, SchemeNumber;
+
+if (typeof require === "undefined") {
+    load("../biginteger.js");
+    load("../schemeNumber.js");
+} else {
+    BigInteger = require('../biginteger').BigInteger;
+    SchemeNumber = require('../schemeNumber').SchemeNumber;
+}
+
+if (typeof print === "undefined")
+    var print = console.log;
+
 var fn = SchemeNumber.fn;
 var isNumber = fn["number?"];
 var ns = fn["number->string"];
