e604299536cef9157219a4e6ad4b07cd2064aa7d
John Tobey <John.Tobey@gmail.com>
2012-04-24 11:35:13 -0400

    __GMP_MP_RELEASE: New constant.

--- src/gmp-constants.h
+++ src/gmp-constants.h
@@ -7,6 +7,7 @@ CONSTANT(mp_bits_per_limb, "mp_bits_per_limb", int)
 CONSTANT(__GNU_MP_VERSION, "__GNU_MP_VERSION", int)
 CONSTANT(__GNU_MP_VERSION_MINOR, "__GNU_MP_VERSION_MINOR", int)
 CONSTANT(__GNU_MP_VERSION_PATCHLEVEL, "__GNU_MP_VERSION_PATCHLEVEL", int)
+CONSTANT(__GMP_MP_RELEASE, "__GMP_MP_RELEASE", int)
 CONSTANT(gmp_version, "gmp_version", stringz)
 #if NPGMP_PORTING
 CONSTANT(__GMP_CC, "__GMP_CC", stringz)
